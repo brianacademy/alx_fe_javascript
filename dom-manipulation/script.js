ac981ff889822e862ecd04ff3b7372ff28c63117
ntabobrian <brianombui254@gmail.com>
2024-08-15 11:28:58 +0300

    Modified to support locat Storage

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -3,7 +3,9 @@ const newQuote = document.getElementById("newQuote");
 const displayQuote = document.getElementById("quoteDisplay");
 const newQuoteText = document.getElementById("newQuoteText");
 const newQuoteCategory = document.getElementById("newQuoteCategory"); 
+const existQuote = document.querySelector('.existQuote');
 
+/*
 quotesArray.push({
   text: "The best way to predict your future is to create it.",
   category: "life"
@@ -16,6 +18,29 @@ quotesArray.push({
   text: "The time is always right to do what is right",
   category: "politics"
 });
+*/
+
+
+const existingQuotes = JSON.parse(localStorage.getItem("quoteText")) || [];
+console.log(existingQuotes);
+existingQuotes.forEach(element => {
+  const para = document.createElement('p');
+  para.innerHTML = JSON.stringify(element);
+
+  existQuote.appendChild(para);
+});
+
+const createAddQuoteForm = () => {
+  const quoteText = newQuoteText.value;
+  const quoteCategory = newQuoteCategory.value;
+
+  quotesArray.push({text: quoteText, category: quoteCategory});
+  newQuoteText.value = "";
+  newQuoteCategory.value = "";
+
+  // saving quotes to localStorage
+  localStorage.setItem("quoteText", JSON.stringify(quotesArray));
+};
 
 const showRandomQuote = () => {  
   // create H3 element which display the quote
@@ -24,14 +49,6 @@ const showRandomQuote = () => {
   h3.innerHTML = randomQuote.text + ` ( ${randomQuote.category} )`;
   displayQuote.appendChild(h3);
   //console.log(quotesArray[Math.floor(Math.random() * quotesArray.length)]);
-};
-
-const createAddQuoteForm = () => {
-  const quoteText = newQuoteText.value;
-  const quoteCategory = newQuoteCategory.value;
-
-  quotesArray.push({text: quoteText, category: quoteCategory});
-
 };
 
 newQuote.addEventListener("click", showRandomQuote);
